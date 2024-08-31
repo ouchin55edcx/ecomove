da2afa1fbe85afb9d2e8d7959cd22454f24ffa40
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-08-31 18:09:53 +0100

    Use Singleton pattern for database connection

--- src/main/java/org/ecomoveV1/Main.java
+++ src/main/java/org/ecomoveV1/Main.java
@@ -1,5 +1,20 @@
 package org.ecomoveV1;
 
+import org.ecomoveV1.config.DatabaseConnection;
+
+import java.sql.Connection;
+
 public class Main {
+
+    public static void main(String[] args){
+        try {
+
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+
+            System.out.println("connected ");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
--- src/main/java/org/ecomoveV1/config/DatabaseConnection.java
+++ src/main/java/org/ecomoveV1/config/DatabaseConnection.java
@@ -4,18 +4,43 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DatabaseConnection {
 
-    private final static String DB_URL = "jdbc:postgresql://localhost:5432/ecomove";
-    private final static String USER = "ecomoveuser";
-    private final static String PASS = "ouchin55edcx";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/ecomove";
+    private static final String USER = "ecomoveuser";
+    private static final String PASS = "ouchin55edcx";
 
 
-    public static void main(String[] args){
+    private static DatabaseConnection instance ;
+    private final Connection connection ;
+
+    private DatabaseConnection(){
         try {
-            Connection connection = DriverManager.getConnection(DB_URL,USER,PASS);
-            System.out.println("Database Connected ");
-            connection.close();
+            connection = DriverManager.getConnection(DB_URL,USER,PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static DatabaseConnection getInstance(){
+        if (instance == null){
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+
+    public void closeConnection(){
+        try {
+            if (connection !=null && !connection.isClosed()){
+                connection.close();
+
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
